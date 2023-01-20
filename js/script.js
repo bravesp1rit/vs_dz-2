d51a09b0016156ce314e92381af66803bb90cf53
brave <progpavlovmark@gmail.com>
2023-01-21 01:04:45 +0200

    dz-2 1.0

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Task #1
+
+let admin;
+let namee = 'Джон';
+admin = namee;
+alert(admin);
+
+// Task #2
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'brave';
+
+// Task #3
+
+let whyString = '10' + 10;
+console.log(typeof(whyString));
+
+// if a number is added to a string, the number will be converted to a string, 
+// regardless of the change in the places of the terms, and result will be num = '1010'
+// and if it were 'hello' + 10, result is 'hello10'
